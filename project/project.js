47491b00bee12b5d472a7192b1d1c92acb7d2a37
SiyankoMaks <112868576+SiyankoMaks@users.noreply.github.com>
2022-12-22 17:13:21 +0300

    Add files via upload

---
+++ project/project.js
@@ -0,0 +1,20 @@
+// Авто-прокрутка слайдера2
+
+$(function(){
+    $(".slayder_body1").jCarouselLite({
+		circular: true,
+		visible: 7,
+        auto: 2000,
+        speed: 1000
+    });
+});
+
+// Авто-прокрутка слайдера 2
+$(function(){
+    $(".slayder_body2").jCarouselLite({
+		circular: true,
+		visible: 8,
+        auto: 2000,
+        speed: 1000
+    });
+});
