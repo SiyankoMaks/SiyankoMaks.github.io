e316626186db57149de2965dd4466fc70d416f39
SiyankoMaks <112868576+SiyankoMaks@users.noreply.github.com>
2023-01-16 18:02:06 +0300

    Update project.js

--- project/project.js
+++ project/project.js
@@ -104,11 +104,11 @@ $("#na, #em, #me, #ph, #check").change(function () {
 // Работа с выпадающей формой с помощью fetch
 
 let newForm = {
-    checkbox: localStorage.getItem("check"),
-    name: localStorage.getItem("f-name"),
+    name: 'Alex',
     phone: localStorage.getItem("Phone"),
     email: localStorage.getItem("Email"),
-    message: localStorage.getItem("comment")
+    message: localStorage.getItem("comment"),
+    checkbox: localStorage.getItem("check")
 }
 
 $("#send11").click(function (e) {
